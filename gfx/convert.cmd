f24a0c05dfd5677a60387d1406d7df22218ddaa0
Marthinus Swart <marthinus.swart@gmail.com>
2026-03-15 22:49:54 +1100

    initial

---
+++ gfx/convert.cmd
@@ -0,0 +1,3 @@
+kingcon image.png ..\image -Interleaved -Format=5 -RawPalette
+kingcon bob.png ..\bob -Interleaved -Format=5 -Mask
+kingcon amiga-bob.png ..\amiga-bob -Interleaved -Format=5 -RawPalette -Mask
